dbdc4a31d7a19c44d73bef4260d519f17b3bb937
Mohamed Safras <awsafras@gmail.com>
2023-04-29 22:30:05 +0530

    service schema is modified

--- models/saloon/service.model.js
+++ models/saloon/service.model.js
@@ -1,32 +1,47 @@
 const mongoose = require("mongoose");
 
-const serviceSchema = new mongoose.Schema({
-  serviceTitle: {
-    type: String,
-    required: true,
+const serviceSchema = new mongoose.Schema(
+  {
+    serviceTitle: {
+      type: String,
+      required: true,
+    },
+    serviceType: {
+      type: String,
+      required: true,
+    },
+    serviceImages: {
+      type: Array,
+      required: false,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "male kids", "female kids"],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    saloon_id: {
+      type: String,
+      required: true,
+    },
+    // style: {
+    //   type: Object,
+    //   required: true,
+    // },
+    description: {
+      type: String,
+      required: false,
+    },
+    serviceTime: {
+      type: Number,
+      required: false,
+    },
+    specialists: Array,
   },
-  serviceType: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female", "male kids", "female kids"],
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  saloon_id: {
-    type: String,
-    required: true,
-  },
-  style: {
-    type: Object,
-    required: true,
-  },
-  specialists: Array,
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("service", serviceSchema);
