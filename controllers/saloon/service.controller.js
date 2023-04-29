d4e484233b4676733eb784624359ba50babf97c0
Mohamed Safras <awsafras@gmail.com>
2023-04-29 22:29:26 +0530

    add service feature is modified

--- controllers/saloon/service.controller.js
+++ controllers/saloon/service.controller.js
@@ -1,33 +1,84 @@
 const serviceModel = require("../../models/saloon/service.model");
 const mongoose = require("mongoose");
+const cloudinary = require("../../utils/cloudinary");
+const validator = require("validator");
 
 const addService = async (req, res) => {
-  const { serviceTitle, serviceType, gender, price, style, specialists } =
+  const { serviceTitle, gender, price, style, specialists, serviceType } =
     req.body;
-
   const saloon_id = req.user;
+  const files = req.files;
+  const serviceImages = [];
 
   try {
     const service = await serviceModel.findOne({ saloon_id, serviceTitle });
 
-    if (service) {
-      return res.status(400).json({
-        message: "You already added like this service",
-        code: 400,
-        status: "failure",
+    if (!service) {
+      if (validator.isEmpty(serviceTitle)) {
+        return res.status(400).json({
+          message: "service name is required",
+          code: 400,
+          status: "failure",
+        });
+      }
+
+      if (validator.isEmpty(serviceType)) {
+        return res.status(400).json({
+          message: "service type is required",
+          code: 400,
+          status: "failure",
+        });
+      }
+
+      for (const file of files) {
+        if (file.path) {
+          await cloudinary.uploader.upload(
+            file.path,
+            {
+              folder: "service",
+              use_filename: false,
+            },
+            (error, result) => {
+              if (error) {
+                console.log(error);
+              } else {
+                serviceImages.push(result.url);
+              }
+            }
+          );
+        }
+      }
+
+      const newSerive = await serviceModel.create({
+        serviceTitle,
+        serviceImages,
+        gender,
+        price,
+        style,
+        specialists,
+        saloon_id,
+        serviceType,
       });
+      return res.status(201).json(newSerive);
     }
 
-    const newSerive = await serviceModel.create({
-      serviceTitle,
-      serviceType,
-      gender,
-      price,
-      style,
-      specialists,
-      saloon_id,
+    return res.status(400).json({
+      message: "You already added like this service",
+      code: 400,
+      status: "failure",
     });
-    return res.status(201).json(newSerive);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+const getAllService = async (req, res) => {
+  const saloon_id = req.user;
+
+  try {
+    const response = await serviceModel.find({ saloon_id });
+
+    return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json(error);
   }
@@ -69,4 +120,5 @@ const deleteService = async (req, res) => {
 module.exports = {
   addService,
   deleteService,
+  getAllService,
 };
