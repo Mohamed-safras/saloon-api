b4fddbeaf9eb9c0a051cb6727373fbc0c2035b46
Mohamed Safras <awsafras@gmail.com>
2023-04-29 22:30:58 +0530

    image upload is added to add service function

--- router/saloon/service.js
+++ router/saloon/service.js
@@ -2,10 +2,19 @@ const express = require("express");
 const {
   addService,
   deleteService,
+  getAllService,
 } = require("../../controllers/saloon/service.controller");
 const requireAuth = require("../../middleware/requireAuth");
+const upload = require("../../utils/upload");
 const saloonserviceRouter = express.Router();
 
-saloonserviceRouter.post("/addService", requireAuth, addService);
+saloonserviceRouter.post(
+  "/addService",
+  requireAuth,
+  upload.array("files"),
+  addService
+);
+
+saloonserviceRouter.get("/getService", requireAuth, getAllService);
 saloonserviceRouter.delete("/removeService/:id", requireAuth, deleteService);
 module.exports = saloonserviceRouter;
